11f2d5d4d9dcb832cadf9c92b069778261090c32
CasparSalaets <160340040+CasparSalaets@users.noreply.github.com>
2025-05-26 16:48:21 +0200

    Small update

--- collections/src/collections/ClientProgram.java
+++ collections/src/collections/ClientProgram.java
@@ -2,9 +2,9 @@ package collections;
 
 import java.util.Iterator;
 
+@SuppressWarnings("rawtypes")
 public class ClientProgram {
 	
-	@SuppressWarnings("rawtypes")
 	public void printAll(Iterator iterator) {
 		while (iterator.hasNext())
 			System.out.println(iterator.next());
@@ -16,13 +16,11 @@ public class ClientProgram {
 	}
 	
 	// When ArrayList and LinkedList implement Iterable the code above can be simplified to
-	@SuppressWarnings("rawtypes")
 	public void printAll1(Iterable iterable) {
 		for (Iterator iterator = iterable.iterator(); iterator.hasNext();)
 			System.out.println(iterator.next());
 	}
 	
-	@SuppressWarnings("rawtypes")
 	public void printBoth1(Iterable collection1, Iterable collection2) {
 		printAll1(collection1);
 		printAll1(collection2);
